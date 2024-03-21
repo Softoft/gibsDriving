fe9af1896b649b7b079a13cc7def751c121bbe4f
Tobi Bueck <tobias.bueck@softoft.de>
2024-03-22 00:11:58 +0100

    changed Design little bit!

--- footer.js
+++ footer.js
@@ -1,5 +1,13 @@
-document.write(`<footer class="bg-light text-center text-lg-start mt-4">
+document.write(`
+    <div class="container text-center my-5">
+        <h2>Contact Gibs\' Gambia DriveHub</h2>
+        <p>For bookings and inquiries: +220 9479117 | <a href="https://www.instagram.com/mrgibsgambia">Instagram</a></p>
+        <a href="https://wa.me/2209479117" class="btn btn-success">Chat on WhatsApp</a>
+        <a href="https://www.instagram.com/mrgibsgambia" class="btn btn-info">Follow on Instagram</a>
+    </div>
+<footer class="bg-light text-center text-lg-start mt-4">
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         © 2024 Gibs' Gambia DriveHub: Your Premier Gambia Driving Service
     </div>
+
 </footer>`)
