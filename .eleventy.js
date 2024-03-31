7a851999b2e04148807a945b8701751aa6995295
Tobi Bueck <tobias.bueck@softoft.de>
2024-04-01 00:55:33 +0200

    deploy!

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,8 @@ module.exports = function(eleventyConfig) {
 	// Copy the `assets` directory to the output
 	eleventyConfig.addPassthroughCopy("images");
 	eleventyConfig.addPassthroughCopy("index.css");
+	eleventyConfig.addPassthroughCopy("index.js");
+	eleventyConfig.addPassthroughCopy("CNAME");
+	eleventyConfig.addPassthroughCopy("google260ae717eb48ea92.html");
+	eleventyConfig.addPassthroughCopy("sitemap.xml");
 };
