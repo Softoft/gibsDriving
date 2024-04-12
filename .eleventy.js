55a9d4b1b254c29ef50cc5623cfdf84e1c3dfd98
Tobi Bueck <tobias.bueck@softoft.de>
2024-04-12 17:19:32 +0200

    new dutch version!

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
+const { EleventyI18nPlugin } = require("@11ty/eleventy");
+
+
 module.exports = function (eleventyConfig) {
-	// Copy the `assets` directory to the output
+	eleventyConfig.addPlugin(EleventyI18nPlugin, {
+		defaultLanguage: "en",
+	});
 	eleventyConfig.addPassthroughCopy("images");
 	eleventyConfig.addPassthroughCopy("index.css");
 	eleventyConfig.addPassthroughCopy("index.js");
