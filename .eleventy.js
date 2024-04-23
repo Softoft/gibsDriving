2e59a8762f5624034b90f2801849b8b9cd0d1a4e
Tobi Bueck <tobias.bueck@softoft.de>
2024-04-23 12:25:08 +0200

    fixed robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,6 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("index.js");
 	eleventyConfig.addPassthroughCopy("fallback-language-redirect.js");
 	eleventyConfig.addPassthroughCopy("CNAME");
-	eleventyConfig.addPassthroughCopy("robots.txt");
 	eleventyConfig.addPassthroughCopy("google260ae717eb48ea92.html");
 	eleventyConfig.addCollection("englishBlogs", function(collectionApi) {
 		return collectionApi.getFilteredByTag("blog").filter((item) => item.page.lang === "en");
